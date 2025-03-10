3f81eefbd67dc99e7cef1afb0afeac59eaa72e43
tinhiu <mypenguinmina@gmail.com>
2025-03-10 09:30:18 +0700

    :rocket fix class3/day7

--- CLASS3/DAY7/script.js
+++ CLASS3/DAY7/script.js
@@ -1,7 +1,84 @@
 // AZUL CODING ---------------------------------------
 // JavaScript - Drag & Drop Files & Elements
 // https://youtu.be/lSCLnWoa6Tw
+var parentElement = document.getElementById('deviceready');
+const wrapper = parentElement.querySelector(".wrapper")
+const svgScene = wrapper.querySelector("svg")
+console.log('svgScene: ', svgScene);
+const content = wrapper.querySelector(".content")
+console.log('content: ', content);
 
+const sources = []
+let currentLine = null
+let drag = false
+wrapper.addEventListener("mousedown", drawStart)
+wrapper.addEventListener("mousemove", drawMove)
+wrapper.addEventListener("mouseup", drawEnd)
+
+wrapper.addEventListener("touchstart", drawStart)
+wrapper.addEventListener("touchmove", drawMove)
+wrapper.addEventListener("touchend", drawEnd)
+
+function deleteLine(e) {
+    let position = e.target.dataset.position
+
+    sources[position].line.remove();
+    sources[position].start.getElementsByClassName("delete")[0].remove()
+    sources[position].end.getElementsByClassName("delete")[0].remove()
+    sources[position] = null
+}
+function drawStart(e) {
+    if (!e.target.classList.contains("hook")) return
+    let eventX = e.type == "mousedown" ? e.clientX - svgScene.offsetLeft : e.targetTouches[0].clientX - svgScene.offsetLeft
+    let eventY = e.type == "mousedown" ? e.clientY - svgScene.offsetTop + window.scrollY : e.targetTouches[0].clientY - scene.offsetTop + window.scrollY
+
+    let lineEl = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+    currentLine = lineEl;
+    currentLine.setAttribute("x1", eventX)
+    currentLine.setAttribute("y1", eventY)
+    currentLine.setAttribute("x2", eventX)
+    currentLine.setAttribute("y2", eventY)
+    currentLine.setAttribute("stroke", "blue")
+    currentLine.setAttribute("stroke-width", "4")
+
+    svgScene.appendChild(currentLine)
+    sources.push({ line: lineEl, start: e.target, end: null })
+
+    drag = true
+}
+function drawMove() {
+    if (!drag || currentLine == null) return
+    let eventX = e.type == "mousedown" ? e.clientX - scene.offsetLeft : e.targetTouches[0].clientX - scene.offsetLeft
+    let eventY = e.type == "mousedown" ? e.clientY - scene.offsetTop + window.scrollY : e.targetTouches[0].clientY - scene.offsetTop + window.scrollY
+    currentLine.setAttribute("x2", eventX)
+    currentLine.setAttribute("y2", eventY)
+}
+
+function drawEnd() {
+    if (!drag || currentLine == null) return
+    let targetHook = e.type == "mouseup" ? e.target : document.elementFromPoint(e.changedTouches[0].clientX, e.changedTouches[0].clientY)
+
+    if (!targetHook.classList.contains("hook") || targetHook == sources[sources.length - 1].start) {
+        currentLine.remove()
+        sources.splice(sources.length - 1, 1)
+    } else {
+        // patience, we'll cover this in a second
+        sources[sources.length - 1].end = targetHook
+
+        let deleteElem = document.createElement("div")
+        deleteElem.classList.add("delete")
+        deleteElem.innerHTML = "&#10005"
+        deleteElem.dataset.position = sources.length - 1
+        deleteElem.addEventListener("click", deleteLine)
+        let deleteElemCopy = deleteElem.cloneNode(true)
+        deleteElemCopy.addEventListener("click", deleteLine)
+
+        sources[sources.length - 1].start.appendChild(deleteElem)
+        sources[sources.length - 1].end.appendChild(deleteElemCopy)
+    }
+    drag = false
+
+}
 // Part 1: Drag & drop files
 
 document.addEventListener("DOMContentLoaded", event => {
